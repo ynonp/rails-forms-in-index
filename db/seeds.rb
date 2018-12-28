532f7dd91b161e6d052fea011e323513a6716bb2
ynonp <ynonperek@gmail.com>
2018-12-28 10:20:14 +0200

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+#
+Reminder.create(text: 'feed some cats', done: false)
+Reminder.create(text: 'listen to the music', done: false)
+Reminder.create(text: 'write blog posts', done: false)
+
---
+++ test/system/reminders_test.rb
@@ -0,0 +1,45 @@
+require "application_system_test_case"
+
+class RemindersTest < ApplicationSystemTestCase
+  setup do
+    @reminder = reminders(:one)
+  end
+
+  test "visiting the index" do
+    visit reminders_url
+    assert_selector "h1", text: "Reminders"
+  end
+
+  test "creating a Reminder" do
+    visit reminders_url
+    click_on "New Reminder"
+
+    fill_in "Done", with: @reminder.done
+    fill_in "Text", with: @reminder.text
+    click_on "Create Reminder"
+
+    assert_text "Reminder was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Reminder" do
+    visit reminders_url
+    click_on "Edit", match: :first
+
+    fill_in "Done", with: @reminder.done
+    fill_in "Text", with: @reminder.text
+    click_on "Update Reminder"
+
+    assert_text "Reminder was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Reminder" do
+    visit reminders_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Reminder was successfully destroyed"
+  end
+end
